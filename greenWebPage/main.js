d552bb39c36e41f996d2420ceeefec6e3daf3968
abol313 <FazlAbol18@gmail.com>
2022-08-13 11:01:34 +0430

    import fonts

--- greenWebPage/main.js
+++ greenWebPage/main.js
@@ -1,1 +1,7 @@
+import './fonts/Yekan-Font-3.0/Yekan.eot'
+import './fonts/Yekan-Font-3.0/Yekan.otf'
+import './fonts/Yekan-Font-3.0/Yekan.ttf'
+import './fonts/Yekan-Font-3.0/Yekan.woff'
+import './fonts/Yekan-Font-3.0/Yekan.woff2'
+import './fonts/Yekan-Font-3.0/Yekan.css'
 import './sass/main.scss'
