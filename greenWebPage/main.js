4cadb39eefdbadeebb5af04867a05d9c60b9fa8d
abol313 <FazlAbol18@gmail.com>
2022-08-13 22:35:53 +0430

    clean code

--- greenWebPage/main.js
+++ greenWebPage/main.js
@@ -4,4 +4,6 @@ import './fonts/Yekan-Font-3.0/Yekan.ttf'
 import './fonts/Yekan-Font-3.0/Yekan.woff'
 import './fonts/Yekan-Font-3.0/Yekan.woff2'
 import './fonts/Yekan-Font-3.0/Yekan.css'
-import './sass/main.scss'
+
+import './sass/main.scss';
+
