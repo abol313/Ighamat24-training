7883dcd5a8c61f55c1f65993a6bf6189208ecc2e
abol313 <FazlAbol18@gmail.com>
2022-08-14 07:31:11 +0430

    add some basic functions and others

---
+++ datePickerJs/script/datePicker.js
@@ -0,0 +1,45 @@
+alert("hello!")
+const datepickers = []
+// every datepicker known as one object literal
+// {
+//     element: //the element of date picker (abol-date-picker)
+//     date: //the date of that
+// }
+
+
+//render for every element that has passed to its class `abol-date-picker`
+function renderDatepicker(element){
+
+}
+
+function getTemplate(){
+
+}
+
+
+//return the number of days of now month
+function getDaysNo(date, datepicker){
+
+}
+
+function chooseDay(day, datepicker){
+
+}
+
+function chooseMonth(month, datepicker){
+
+}
+
+
+function chooseYearTab(datepicker){
+
+}
+
+function chooseMonthTab(datepicker){
+
+}    
+
+function chooseDayTab(datepicker){
+
+}
+
