ce7df1ba3bcf1ef4ee5b6c0e0a94a080c8b73620
abol313 <FazlAbol18@gmail.com>
2022-08-15 20:17:14 +0430

    handle elements

--- localStorageJs/script/script.js
+++ localStorageJs/script/script.js
@@ -1,6 +1,19 @@
 import PresentTime from './PresentTime'
 
-let presentTime = new PresentTime()
+const presentTimeUlEl = document.querySelector('main .present-time-container .present-time-ul')
+const inputEnterTimeEl = document.querySelector('form #enter_time')
+const inputExitTimeEl = document.querySelector('form #exit_time')
+const submitButton = document.querySelector('form #submit_present_time')
 
-console.log(PresentTime.presentTimes)
-console.log(presentTime)
+submitButton.addEventListener('click', submitPresentTime)
+
+function submitPresentTime(){
+    let presentTime = new PresentTime(
+        inputEnterTimeEl.value,
+        inputExitTimeEl.value
+    )
+
+    presentTime.makeElement(presentTimeUlEl)
+
+    console.log(presentTime)
+}
