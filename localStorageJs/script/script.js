fcf2a47f5972b3d262ac55d7fd3865fbb67e9ce0
abol313 <FazlAbol18@gmail.com>
2022-08-15 23:40:37 +0430

    handle localstorage api

--- localStorageJs/script/script.js
+++ localStorageJs/script/script.js
@@ -7,6 +7,8 @@ const submitButton = document.querySelector('form #submit_present_time')
 
 submitButton.addEventListener('click', submitPresentTime)
 
+storagePreparePresentTimes()
+
 function submitPresentTime(){
     let presentTime = new PresentTime(
         inputEnterTimeEl.value,
@@ -15,5 +17,26 @@ function submitPresentTime(){
 
     presentTime.makeElement(presentTimeUlEl)
 
+    storagePushPresentTime(presentTime)
     console.log(presentTime)
+}
+
+function storagePreparePresentTimes(){
+    // debugger
+    let presentTimes = window.localStorage.getItem('presentTimes')
+    if(!presentTimes)return false
+
+    presentTimes = JSON.parse(presentTimes)
+
+    for(let presentTime of presentTimes)
+        new PresentTime(presentTime.enterTime, presentTime.exitTime)
+            .makeElement(presentTimeUlEl)
+            .setSubmitDate(new Date(presentTime.submitDate))
+}
+
+function storagePushPresentTime(presentTime){
+    let presentTimes = window.localStorage.getItem('presentTimes')
+    presentTimes = presentTimes ? JSON.parse(presentTimes) : []
+    presentTimes.push(presentTime)
+    window.localStorage.setItem('presentTimes', JSON.stringify(presentTimes))
 }
