0d5b7ef6641b49a4f0976fa0c068d2dbe6713eac
abol313 <FazlAbol18@gmail.com>
2022-08-16 11:25:23 +0430

    return inner elements in Alert

--- localStorageJs/script/Alert.js
+++ localStorageJs/script/Alert.js
@@ -6,6 +6,8 @@ export default class Alert{
     mode
     boxElement
     tempElement
+    titleElement
+    descriptionElement
 
     constructor(title, description, mode='ok', boxElement=document.body){
         this.title = title
@@ -14,10 +16,33 @@ export default class Alert{
         this.boxElement = boxElement
     }
 
+    setTitle(title){
+        this.title = title
+        this.titleElement.innerTEXT = this.title
+        return this
+    }
+
+    setDescription(decription){
+        this.description = this.description
+        this.descriptionElement.innerTEXT = this.description
+        return this
+    }
+
     make(){
         this.tempElement = document.createElement('div')
-        this.boxElement.appendChild( this.tempElement )
+        this.titleElement = document.createElement('h2')
+        this.descriptionElement = document.createElement('p')
+
         this.tempElement.classList.add('abol-alert-box'+'-'+this.mode)
+        this.titleElement.classList.add('title')
+        this.descriptionElement.classList.add('description')
+
+        this.setTitle(this.title)
+        this.setDescription(this.description)
+
+        this.boxElement.appendChild( this.tempElement )
+        this.tempElement.appendChild( this.titleElement )
+        this.descriptionElement.appendChild( this.descriptionElement )
 
         return this
     }
