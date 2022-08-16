ef9b7b8310843c09ea18218b743bb67e6e3cf6f8
abol313 <FazlAbol18@gmail.com>
2022-08-16 10:53:15 +0430

    handle clear all present items

--- localStorageJs/script/script.js
+++ localStorageJs/script/script.js
@@ -4,10 +4,14 @@ const presentTimeTableEl = document.querySelector('main .present-time-container 
 const inputEnterTimeEl = document.querySelector('form #enter_time')
 const inputExitTimeEl = document.querySelector('form #exit_time')
 const submitButton = document.querySelector('form #submit_present_time')
+const clearButton = document.querySelector('form #clear_present_times')
 
 submitButton.addEventListener('click', submitPresentTime)
+clearButton.addEventListener('click', clearPresentTimes)
 
 storagePreparePresentTimes()
+
+makeNRandomlyPresentTimes(1000)
 
 function submitPresentTime(){
     let presentTime = new PresentTime(
@@ -19,6 +23,11 @@ function submitPresentTime(){
 
     storagePushPresentTime(presentTime)
     console.log(presentTime)
+}
+
+function clearPresentTimes(){
+    window.localStorage.clear()
+    window.location.reload()
 }
 
 function storagePreparePresentTimes(){
@@ -39,4 +48,18 @@ function storagePushPresentTime(presentTime){
     presentTimes = presentTimes ? JSON.parse(presentTimes) : []
     presentTimes.push(presentTime)
     window.localStorage.setItem('presentTimes', JSON.stringify(presentTimes))
+}
+
+function makeNRandomlyPresentTimes(number){
+    for(let i=0; i<number; i++){
+        const entryTime = makeRandomlyDate().toLocaleDateString('en-ZA')
+        const exitTime = makeRandomlyDate().toLocaleDateString('en-ZA')
+
+        let presentTime = new PresentTime(entryTime, exitTime)
+        presentTime.makeElement(presentTimeTableEl)
+    } 
+}
+
+function makeRandomlyDate(){
+    return new Date(parseInt(Math.random()*1000+1970), parseInt(Math.random()*12), parseInt(Math.random()*29))
 }
