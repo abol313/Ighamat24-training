5fbbcf43f5e75839d576c971360a547375fb8ef8
abol313 <FazlAbol18@gmail.com>
2022-08-17 09:40:25 +0430

    add gallery sfc (single-file component)

---
+++ galleryVueJs/src/components/Gallery.vue
@@ -0,0 +1,57 @@
+<template>
+    <h1>
+        Welcome To Your Gallery !
+    </h1>
+    <div class="images-box">
+        <div class="image"><img src="../assets/images/abol_roocket_profile.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abstract_example_shot.png" alt=""></div>
+        <div class="image"><img src="../assets/images/css_css.png" alt=""></div>
+        <div class="image"><img src="../assets/images/first_week_day_of_month.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation_custom_error_message.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abol_roocket_profile.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abstract_example_shot.png" alt=""></div>
+        <div class="image"><img src="../assets/images/css_css.png" alt=""></div>
+        <div class="image"><img src="../assets/images/first_week_day_of_month.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation_custom_error_message.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abol_roocket_profile.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abstract_example_shot.png" alt=""></div>
+        <div class="image"><img src="../assets/images/css_css.png" alt=""></div>
+        <div class="image"><img src="../assets/images/first_week_day_of_month.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation_custom_error_message.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abol_roocket_profile.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abstract_example_shot.png" alt=""></div>
+        <div class="image"><img src="../assets/images/css_css.png" alt=""></div>
+        <div class="image"><img src="../assets/images/first_week_day_of_month.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation_custom_error_message.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abol_roocket_profile.png" alt=""></div>
+        <div class="image"><img src="../assets/images/abstract_example_shot.png" alt=""></div>
+        <div class="image"><img src="../assets/images/css_css.png" alt=""></div>
+        <div class="image"><img src="../assets/images/first_week_day_of_month.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation.png" alt=""></div>
+        <div class="image"><img src="../assets/images/laravel_routing_validation_custom_error_message.png" alt=""></div>
+
+    </div>
+</template>
+
+<style scoped>
+
+    .images-box{
+        display:flex;
+        flex-wrap:wrap;
+        gap:20px;
+    }
+
+    .images-box .image{
+        position: relative;
+        width:300px;
+        flex-grow: 1;
+    }
+
+    .images-box .image img{
+        width: 100%;
+    }
+</style>
