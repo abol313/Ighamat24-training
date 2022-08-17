1948dfd8d11d7670ee0a5805d8d25c3a98411918
abol313 <FazlAbol18@gmail.com>
2022-08-17 09:39:57 +0430

    add router

--- galleryVueJs/src/main.js
+++ galleryVueJs/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import router from './router/index.js'
+
 import './assets/main.scss'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
