0a132dabb7a23e1802e54fa6a29354db927b18a9
abol313 <FazlAbol18@gmail.com>
2022-08-17 09:40:37 +0430

    add Home sfc

---
+++ galleryVueJs/src/components/Home.vue
@@ -0,0 +1,21 @@
+<script>
+import CustomButton from './CustomButton.vue'
+
+export default {
+    components:{
+        CustomButton
+    }
+}
+</script>
+
+<template>
+    <router-link to="/gallery">
+        <CustomButton class="go-on-gallery">
+            Go To Gallery
+        </CustomButton>
+    </router-link>
+<!-- 
+    <router-view>
+        
+    </router-view> -->
+</template>
