b6079e942736b4bab1b594ded7ab1fb8e9352c83
abol313 <FazlAbol18@gmail.com>
2022-08-18 22:09:07 +0430

    import CreateTodoView

--- TodoVueJs/src/router/index.js
+++ TodoVueJs/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CreateTodoView from '../views/CreateTodoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,7 @@ const router = createRouter({
     {
       path: '/new',
       name: 'new',
-      component: TodoView
+      component: CreateTodoView
     },
     
   ]
