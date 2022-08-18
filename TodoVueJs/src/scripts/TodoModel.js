ee2995ad0995e55ebd8ab2cc9ce40a511c3a3100
abol313 <FazlAbol18@gmail.com>
2022-08-18 14:39:30 +0430

    import StorageDB and handle the model

--- TodoVueJs/src/scripts/TodoModel.js
+++ TodoVueJs/src/scripts/TodoModel.js
@@ -1,3 +1,58 @@
+import StorageDB from "../modules/StorageDB";
 export default class TodoModel {
+    storageDB;
+    static table = 'todos';
+    static password = '12345678';
+    static fields = [
+        'id',
+        'title',
+        'description',
+
+        'due_at',
+        'done_at',
+        'created_at',
+        'updated_at',
+    ];
     
+    /**
+     * 
+     * @param {StorageDB} storageDB 
+     */
+    constructor(storageDB){
+        this.storageDB = storageDB;
+    }
+
+    getDB(){
+        return this.storageDB;
+    }
+
+    useTable(){
+        if(this.getDB().useTable(this.table) === undefined)
+            this.getDB().createTable(this.table, this.fields);
+
+        return this.getDB().useTable(this.table);
+    }
+
+
+    static connect(){
+        let tableModel = new TodoModel(new StorageDB(window.localStorage, 'todo_db', this.password));
+        
+        return tableModel;
+    }
+
+    static create(record){
+        let tableModel = this.connect();
+        tableModel.getDB()
+            .useTable('todos')
+            .insert({
+
+            });
+        
+        return tableModel;
+    }
+
+    static query(){
+        return this.useTable().query();
+    }
+
 }
