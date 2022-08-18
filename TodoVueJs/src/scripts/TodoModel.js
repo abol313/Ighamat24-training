b6a0bfbe9aee44dd7025afd9f9efbee2951d9935
abol313 <FazlAbol18@gmail.com>
2022-08-18 14:46:36 +0430

    add db property and comment to info of types

--- TodoVueJs/src/scripts/TodoModel.js
+++ TodoVueJs/src/scripts/TodoModel.js
@@ -1,6 +1,8 @@
-import StorageDB from "../modules/StorageDB";
+import StorageDB,{StorageTable,QueryBuilder} from "../modules/StorageDB";
 export default class TodoModel {
     storageDB;
+    static db = 'todo_db';
+
     static table = 'todos';
     static password = '12345678';
     static fields = [
@@ -22,10 +24,18 @@ export default class TodoModel {
         this.storageDB = storageDB;
     }
 
+    /**
+     * 
+     * @returns {StorageDB} returns related database
+     */
     getDB(){
         return this.storageDB;
     }
 
+    /**
+     * 
+     * @returns {StorageTable} returns related table
+     */
     useTable(){
         if(this.getDB().useTable(this.table) === undefined)
             this.getDB().createTable(this.table, this.fields);
@@ -33,13 +43,21 @@ export default class TodoModel {
         return this.getDB().useTable(this.table);
     }
 
-
+    /**
+     * 
+     * @returns {TodoModel} connects to the db and table then returns itself instance
+     */
     static connect(){
-        let tableModel = new TodoModel(new StorageDB(window.localStorage, 'todo_db', this.password));
+        let tableModel = new TodoModel(new StorageDB(window.localStorage, this.db, this.password));
         
         return tableModel;
     }
 
+    /**
+     * 
+     * @param {object} record gets the record key values to insert on table
+     * @returns {TodoModel}
+     */
     static create(record){
         let tableModel = this.connect();
         tableModel.getDB()
@@ -51,6 +69,10 @@ export default class TodoModel {
         return tableModel;
     }
 
+    /**
+     * 
+     * @returns {QueryBuilder} returns the query builder to pass queries conditions and etc.
+     */
     static query(){
         return this.useTable().query();
     }
