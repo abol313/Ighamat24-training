62f69c343348791223b93746a6c6ba51a72c744d
abol313 <FazlAbol18@gmail.com>
2022-08-18 07:40:16 +0430

    Create the Todo Vue Js App

---
+++ TodoVueJs/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ TodoVueJs/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    
+    {
+      path: '/new',
+      name: 'new',
+      component: TodoView
+    },
+    
+  ]
+})
+
+export default router
