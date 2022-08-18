d139b8e77b15c3735a13eb2b473c25b768ada03a
abol313 <FazlAbol18@gmail.com>
2022-08-18 22:08:46 +0430

    import script.js

--- TodoVueJs/src/main.js
+++ TodoVueJs/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './scripts/script.js'
+
 import './assets/main.css'
 
 const app = createApp(App)
