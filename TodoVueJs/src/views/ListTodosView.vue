4c091e5177a57ca41dea69c49af5ec7b956fb5f1
abol313 <FazlAbol18@gmail.com>
2022-08-19 18:03:22 +0430

    query all todos from StorageDB>TodoModel and make todo sfc-es

---
+++ TodoVueJs/src/views/ListTodosView.vue
@@ -0,0 +1,30 @@
+<script>
+import Todo from '../components/Todo.vue';
+import TodoModel from '../scripts/TodoModel.js';
+export default {
+    data(){
+        return {
+            todos: TodoModel.all()
+
+        };
+    },
+    components:{
+        Todo
+    }
+}
+</script>
+
+<template>
+
+    <Todo 
+        v-for="todo in todos"
+        :id="todo.id"
+        :title="todo.title"
+        :description="todo.description"
+        :due-at="new Date(todo.due_at)"
+        :done-at="new Date(todo.done_at)"
+        
+        :created-at="new Date(todo.created_at)"
+        :updated-at="new Date(todo.updated_at)"
+    />
+</template>
