68dafce1e553e1441d2322bdbbdbf933b11b38c1
abol313 <FazlAbol18@gmail.com>
2022-08-19 17:29:50 +0430

    handle Todo sfc single-file component

--- TodoVueJs/src/components/Todo.vue
+++ TodoVueJs/src/components/Todo.vue
@@ -1,6 +1,20 @@
 <script>
 
 export default {
+
+
+    // data(){
+    //     return {
+    //         id: 1,
+    //         title: 'Todo title',
+    //         description: 'Todo description..',
+    //         created_at: new Date('2022-08-10T12:37:36.110Z'),
+    //         updated_at: new Date('2022-08-16T12:37:36.110Z'),
+    //         done_at: null,
+    //         status: false,
+    //     };
+    // },
+
     props: [
         'id',
         'title',
@@ -10,21 +24,44 @@ export default {
         'status',
     ],
 
-    // data(){
-    //     return {
-    //         id: 1,
-    //         title: 'Todo title',
-    //         description: 'Todo description..',
-    //         created_at: new Date(),
-    //         updated_at: new Date(),
-    //         done_at: null,
-
-    //     };
-    // },
-
     computed:{
         last_edit(){
-            return this.updated_at || this.created_at;
+            let date = this.updated_at || this.created_at;
+            if(!date) return date;
+            console.log(date);
+            let nowDate = new Date();
+            
+            let last_edit_out = '';
+            switch(true){
+                case nowDate.getFullYear() != date.getFullYear():
+                    last_edit_out += `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`;
+                    break;
+
+                case nowDate.getMonth() != date.getMonth():
+                    let diffMonth = nowDate.getMonth() - date.getMonth();
+                    last_edit_out += `${diffMonth} month${diffMonth>1?'s':''} ago`;
+                    break;
+                
+                case nowDate.getHours() != date.getHours():
+                    let diffHour = nowDate.getHours() - date.getHours();
+                    last_edit_out += `${diffHour} hour${diffHour>1?'s':''} ago`;
+                    break;
+
+                case nowDate.getMinutes() != date.getMinutes():
+                    let diffMinute = nowDate.getMinutes() - date.getMinutes();
+                    last_edit_out += `${diffMinute} minute${diffMinute>1?'s':''} ago`;
+                    break;
+                
+                case nowDate.getSeconds() != date.getSeconds():
+                    let diffSecond = nowDate.getSeconds() - date.getSeconds();
+                    last_edit_out += `${diffSecond} second${diffSecond>1?'s':''} ago`;
+                    break;
+                
+                default:
+                    last_edit_out+= 'now';
+            }
+
+            return last_edit_out;
         }
     },
 
@@ -32,7 +69,8 @@ export default {
         toggleStatus(){
             this.status = !this.status;
         }
-    }
+    },
+
 }
 </script>
 
@@ -46,7 +84,7 @@ export default {
 </template>
 
 <style scoped>
-    .todo-item >>> * {
+    .todo-item {
         border:1px solid black;
     }
 </style>
