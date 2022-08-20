8e659e85a9ceaf9d10d73af8e73e06fc52ca1bd3
abol313 <FazlAbol18@gmail.com>
2022-08-20 10:23:07 +0430

    create CreateFormTodo sfc to create the todo

---
+++ TodoVueJs/src/components/FormCreateTodo.vue
@@ -0,0 +1,41 @@
+<script>
+import TodoModel from '../scripts/TodoModel';
+
+export default{
+    data(){
+        return{
+            title:'',
+            description:'',
+            dueAt:null,
+        }
+    },
+    methods:{
+        createTodo(){
+            TodoModel.create({
+                title: this.title,
+                description: this.description,
+                due_at: this.dueAt,
+                done_at: null,
+                
+                created_at: new Date().toISOString(),
+                updated_at: null,
+            });
+        },
+        resetData(){
+            this.title = '';
+            this.description = '';
+            this.dueAt = null
+        }
+    }
+}
+</script>
+
+<template>
+    <form class="form-box">
+        <input class="title" v-model="title" required/>
+        <input class="description" v-model="description" required/>
+        <input type="datetime-local" class="due_at" v-model="dueAt" required/>
+
+        <button @click="createTodo" type="button"></button>
+    </form>
+</template>
