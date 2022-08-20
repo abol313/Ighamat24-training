dea049915a8b75bc13ae81d43ae2612c512fcf12
abol313 <FazlAbol18@gmail.com>
2022-08-20 09:02:05 +0430

    fix the changed attrs

--- TodoVueJs/src/components/Todo.vue
+++ TodoVueJs/src/components/Todo.vue
@@ -5,30 +5,34 @@ import TodoModel from '../scripts/TodoModel';
 export default {
 
 
-    props: ['todo']
+    props: {
+        'todo':Object,
+    }
     ,
 
     data(){
         return {
-            id:this.todo.id,
-            title:this.todo.title,
-            description:this.todo.description,
-            dueAt:this.todo.due_at && new Date(this.todo.due_at),
-            doneAt:this.todo.done_at && new Date(this.todo.done_at),
+            id:this.$props.todo.id,
+            title:this.$props.todo.title,
+            description:this.$props.todo.description,
+            dueAt:this.$props.todo.due_at && new Date(this.$props.todo.due_at),
+            doneAt:this.$props.todo.done_at && new Date(this.$props.todo.done_at),
 
-            createdAt:this.todo.created_at && new Date(this.todo.created_at),
-            updatedAt:this.todo.updated_at && new Date(this.todo.updated_at),
+            createdAt:this.$props.todo.created_at && new Date(this.$props.todo.created_at),
+            updatedAt:this.$props.todo.updated_at && new Date(this.$props.todo.updated_at),
         }
     },
 
     computed:{
         status(){
+            
+            console.log('status:',this.doneAt,!!this.doneAt);
             return !!this.doneAt;
         },
         last_edit(){
             let date = this.updatedAt || this.createdAt;
             if(!date) return date;
-            console.log(date);
+            // console.log(date);
             let nowDate = new Date();
             
             let last_edit_out = '';
@@ -73,9 +77,24 @@ export default {
     methods:{
         toggleStatus(){
             this.doneAt = this.doneAt? null: new Date();
+            this.$emit('change-todo-status');
             TodoModel.update(this.id, {done_at: this.doneAt});
         }
     },
+
+    watch:{
+        todo(todo){
+            this.id=todo.id;
+            this.title=todo.title;
+            this.description=todo.description;
+            this.dueAt=todo.due_at && new Date(todo.due_at);
+            this.doneAt=todo.done_at && new Date(todo.done_at);
+
+            this.createdAt=todo.created_at && new Date(todo.created_at);
+            this.updatedAt=todo.updated_at && new Date(todo.updated_at);
+        }
+    }
+
 
 }
 </script>
@@ -85,7 +104,7 @@ export default {
         <h3 class="title">{{title}}</h3>
         <p class="description">{{description}}</p>
         <p class="last-edit">{{last_edit}}</p>
-        <p :class="{'status':true, 'status-done':this.status}" @click="toggleStatus()"></p>
+        <p :class="{'status':true, 'status-done':!!this.doneAt}" @click="toggleStatus()">{{this.doneAt}}</p>
     </div>
 </template>
 
