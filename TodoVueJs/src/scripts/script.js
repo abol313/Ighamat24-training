e03c9eae9ac2eaa53ec207a93ac8ce9f21d4efae
abol313 <FazlAbol18@gmail.com>
2022-08-20 17:16:59 +0430

    fix tests

--- TodoVueJs/src/scripts/script.js
+++ TodoVueJs/src/scripts/script.js
@@ -1,12 +1,24 @@
 import TodoModel from './TodoModel.js';
+
 window.TodoModel = TodoModel;
 
-console.log(TodoModel.create({id:1, title:'go to the gym'}));
-console.log(TodoModel.create({id:2, title:'some to the jam'}));
-console.log(TodoModel.create({id:3, title:'hello tu the jjk'}));
-console.log(TodoModel.create({id:4, title:'shallow get gym'}));
-console.log(TodoModel.all());
-console.log(TodoModel.query());
-console.log(TodoModel.query().whereIn('id',[1,2,3,4,5,6]));
-console.log(TodoModel.create({id:5, title:'okok i0'}));
+// for(let i=0; i<10; i++)
+//     console.log(TodoModel.create({
+//         // id:i,
+//         title:'title'+i,
+//         description:'description'+i,
+//         due_at: makeRandomlyDate(2022),
+//         done_at: makeRandomlyDate(2022),
+
+//         created_at: makeRandomlyDate(2022),
+//         updated_at: makeRandomlyDate(2022),
+//     }));
+
+function makeRandomlyDate(year=null , month=null, day=null){
+    return new Date(parseInt(year || Math.random()*1000+1971), parseInt(month || Math.random()*12), parseInt(day || Math.random()*29))
+}
+// console.log(TodoModel.all());
+// console.log(TodoModel.query());
+// console.log(TodoModel.query().whereIn('id',[1,2,3,4,5,6]));
+// console.log(TodoModel.create({id:5, title:'okok i0'}));
 
