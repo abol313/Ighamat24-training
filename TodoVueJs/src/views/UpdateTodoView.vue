cec0424acdf5189aeecd1797cde0ffc8ad52e449
abol313 <FazlAbol18@gmail.com>
2022-08-20 16:51:54 +0430

    add UpdateTodoView to have a page od updating the todo

---
+++ TodoVueJs/src/views/UpdateTodoView.vue
@@ -0,0 +1,20 @@
+<script>
+import FormUpdateTodo from "../components/FormUpdateTodo.vue";
+import TodoModel from '../scripts/TodoModel.js';
+
+export default {
+    data(){
+        return{
+            todo: TodoModel.query().where('id', this.$route.params.id).get()[0]
+        }
+    },
+    components:{
+        FormUpdateTodo
+    }
+}
+
+</script>
+
+<template>
+    <FormUpdateTodo :todo="todo"/>
+</template>
