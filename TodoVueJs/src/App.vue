08fa42ce6275f1f448020d6f4c0368e6e86f57d1
abol313 <FazlAbol18@gmail.com>
2022-08-20 17:03:35 +0430

    add navigation to navigate to routes

--- TodoVueJs/src/App.vue
+++ TodoVueJs/src/App.vue
@@ -5,7 +5,22 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <h1>Welcome To Todo Proj !</h1>
+  
   </header>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/new">
+          <h2>Create A Todo</h2>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/">
+          <h2>List Todos</h2>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
 
   <RouterView />
 </template>
