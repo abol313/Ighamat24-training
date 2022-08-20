b6e2c4e7a5d6ecbc6d33df87ce71c5e20efe2893
abol313 <FazlAbol18@gmail.com>
2022-08-20 09:02:58 +0430

    add filter todos

--- TodoVueJs/src/views/ListTodosView.vue
+++ TodoVueJs/src/views/ListTodosView.vue
@@ -1,23 +1,53 @@
 <script>
 import Todo from '../components/Todo.vue';
 import TodoModel from '../scripts/TodoModel.js';
+import FilterTodo from '../components/FilterTodo.vue';
 export default {
     data(){
         return {
-            todos: TodoModel.all()
-
+            orgTodos: TodoModel.all(),
+            todos: this.orgTodos
         };
     },
+    mounted(){
+        this.setDataUndones();
+    },
     components:{
-        Todo
-    }
+        Todo,
+        FilterTodo
+    },
+    methods:{
+        setDataDones(){
+            console.log('set data dones...');
+            this.todos = this.orgTodos.filter(todo=> todo.done_at!==null);
+            console.log('setted.');
+        },
+        setDataUndones(){
+            this.todos = this.orgTodos.filter(todo=> todo.done_at===null);
+        },
+        setDataAll(){
+            this.todos = this.orgTodos;
+        }
+    },
+    emits:[
+        'no-block',
+        'done-block',
+        'undone-block',
+    ],
+
 }
 </script>
 
 <template>
 
+    <FilterTodo @no-block="setDataAll()"
+    @done-block="setDataUndones()"
+    @undone-block="setDataDones()"
+    />
+    
     <Todo 
         v-for="todo in todos"
         :todo="todo"
+        :todo-id="todo.id"
     />
 </template>
