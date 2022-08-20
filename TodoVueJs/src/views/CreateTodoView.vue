0df231fa6d0c7a43c00dbc9c6a1120b30e4a13ed
abol313 <FazlAbol18@gmail.com>
2022-08-20 10:23:22 +0430

    handle CreateTodoVIew

---
+++ TodoVueJs/src/views/CreateTodoView.vue
@@ -0,0 +1,13 @@
+<script>
+import FormCreateTodo from "../components/FormCreateTodo.vue";
+
+export default {
+    components:{
+        FormCreateTodo
+    }
+}
+</script>
+
+<template>
+    <FormCreateTodo/>
+</template>
