5b4511d63c50bfe4a22cbe5c3a09532800ed8ef1
abol313 <FazlAbol18@gmail.com>
2022-08-20 09:38:01 +0430

    handle on change todo status

--- TodoVueJs/src/views/ListTodosView.vue
+++ TodoVueJs/src/views/ListTodosView.vue
@@ -6,7 +6,8 @@ export default {
     data(){
         return {
             orgTodos: TodoModel.all(),
-            todos: this.orgTodos
+            todos: this.orgTodos,
+            filterMode: 'undones',
         };
     },
     mounted(){
@@ -18,21 +19,36 @@ export default {
     },
     methods:{
         setDataDones(){
-            console.log('set data dones...');
             this.todos = this.orgTodos.filter(todo=> todo.done_at!==null);
-            console.log('setted.');
+            this.filterMode = 'dones';
         },
         setDataUndones(){
             this.todos = this.orgTodos.filter(todo=> todo.done_at===null);
+            this.filterMode = 'undones';
         },
         setDataAll(){
             this.todos = this.orgTodos;
+            this.filterMode = 'all';
+        },
+        onChangeTodoStatus(todo, isDone){
+            todo.done_at = isDone? new Date(): null;
+            if(this.filterMode=='all')return;
+
+            if(this.filterMode=='dones' && !isDone){
+                this.todos = this.todos.filter(aTodo=> aTodo!=todo);
+            }
+            if(this.filterMode=='undones' && isDone){
+                console.log('changed todo statyus :',todo)
+                this.todos = this.todos.filter(aTodo=> aTodo!=todo);
+            }
+
         }
     },
     emits:[
         'no-block',
         'done-block',
         'undone-block',
+        'change-todo-status'
     ],
 
 }
@@ -48,6 +64,6 @@ export default {
     <Todo 
         v-for="todo in todos"
         :todo="todo"
-        :todo-id="todo.id"
+        @change-todo-status="onChangeTodoStatus"
     />
 </template>
