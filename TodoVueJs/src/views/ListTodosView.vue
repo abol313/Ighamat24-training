7be5925ffa9c1558473beb83c90ad2ac03fa14f2
abol313 <FazlAbol18@gmail.com>
2022-08-22 09:04:52 +0430

    handle new logic of filtering

--- TodoVueJs/src/views/ListTodosView.vue
+++ TodoVueJs/src/views/ListTodosView.vue
@@ -7,103 +7,52 @@ export default {
         return {
             orgTodos: TodoModel.all(),
             todos: this.orgTodos,
-            filterMode: 'undones',
-            filterOrderIsDesc: true,
             filterSearch: null,
         };
     },
     mounted(){
-        this.setDataUndones();
-        this.reorderDesc();
+
     },
     components:{
         Todo,
         FilterTodo
     },
     methods:{
-        setDataDones(){
-            this.todos = this.orgTodos.filter(todo=> todo.done_at!==null);
-            this.filterMode = 'dones';
-        },
-        setDataUndones(){
-            this.todos = this.orgTodos.filter(todo=> todo.done_at===null);
-            this.filterMode = 'undones';
-        },
-        setDataAll(){
-            this.todos = this.orgTodos;
-            this.filterMode = 'all';
-        },
+
 
         onChangeTodoStatus(todo, isDone){
-            todo.done_at = isDone? new Date(): null;
-            if(this.filterMode=='all')return;
+            // todo.done_at = isDone? new Date(): null;
+            // if(this.filterMode=='all')return;
 
-            if(this.filterMode=='dones' && !isDone){
-                this.todos = this.todos.filter(aTodo=> aTodo!=todo);
-            }
-            if(this.filterMode=='undones' && isDone){
-                console.log('changed todo statyus :',todo)
-                this.todos = this.todos.filter(aTodo=> aTodo!=todo);
-            }
-
-        },
-
-        reorderAsc(){
-            this.filterOrderIsDesc = false;
-        },
-        reorderDesc(){
-            this.filterOrderIsDesc = true;
-        },
-        reorder(){
-            this.todos.sort(this.reorderFuncBaseOfLastEditTime);
-        },
-        reorderFuncBaseOfLastEditTime(todoA, todoB, isDesc=this.filterOrderIsDesc){
-            let todoADate = new Date(todoA.updated_at || todoA.created_at);
-            let todoBDate = new Date(todoB.updated_at || todoB.created_at);
-
-            if(isDesc)
-                return todoADate.getTime() > todoBDate.getTime() ? -1:1;
-            else
-                return todoADate.getTime() < todoBDate.getTime() ? -1:1;
-
+            // if(this.filterMode=='dones' && !isDone){
+            //     this.todos = this.todos.filter(aTodo=> aTodo!=todo);
+            // }
+            // if(this.filterMode=='undones' && isDone){
+            //     console.log('changed todo statyus :',todo)
+            //     this.todos = this.todos.filter(aTodo=> aTodo!=todo);
+            // }
+            
         },
 
-        setSearch(title){
-            title = title.trim();
-            this.filterSearch = title==''? null : title ;
-        },
-        search(){
-            let regex = new RegExp(this.filterSearch.replaceAll(/\s/g,'').split('').join('.*'),'i');
-            return this.todos.filter(todo=> regex.test(todo.title));
-        },
+
+
         filter(filterCallback){
             console.log('filter...');
             this.todos = filterCallback(this.orgTodos);
+        },
+        setSearch(searchText){
+            this.filterSearch = searchText;
         }
     },
     watch:{
-        filterOrderIsDesc(isDescNew){
-            this.reorder();
-        },
-        todos(){
-            this.reorder();
-        }
+
     },
     computed:{
         getTodos(){
-            if(this.filterSearch == null)
-                return this.todos;
-            return this.search();
+            return this.todos;
         }
     },
     emits:[
-        'no-block',
-        'done-block',
-        'undone-block',
-
-        'reorder-desc',
-        'reorder-asc',
-
         'filter',
         'search',
 
@@ -117,9 +66,6 @@ export default {
 
     <FilterTodo 
     @filter="filter"
-
-    @reorder-desc="reorderDesc"
-    @reorder-asc="reorderAsc"
 
     @search="setSearch"
     />
