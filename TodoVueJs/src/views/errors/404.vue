2ac709e0905d11df68ab090cf9d552c1840e490d
abol313 <FazlAbol18@gmail.com>
2022-08-22 12:52:53 +0430

    add 404 error view

---
+++ TodoVueJs/src/views/errors/404.vue
@@ -0,0 +1,17 @@
+<script>
+import ExclamationLogo from '../../components/icons/exclamation.vue';
+
+export default {
+    components:{
+        ExclamationLogo,
+    }
+}
+</script>
+
+<template>
+    <div class="error-404">
+        <h1>404</h1>
+        <ExclamationLogo class="error-logo"/>
+        <h1>Page Not Found</h1>
+    </div>
+</template>
