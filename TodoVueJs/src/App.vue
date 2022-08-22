53fb34d2be4ae64b7efa553e66ac450c7a43080a
abol313 <FazlAbol18@gmail.com>
2022-08-22 16:35:30 +0430

    remove header and nav when is in the 404 page

--- TodoVueJs/src/App.vue
+++ TodoVueJs/src/App.vue
@@ -1,13 +1,34 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+
+
+export default {
+  data(){
+    return {
+      isNotFoundPage: false
+    }
+  },
+
+  components:{
+    RouterLink,
+    RouterView,
+  },
+  watch:{
+    $route(newRoute){
+      // console.log(newRoute.redirectedFrom);
+      
+      this.isNotFoundPage = newRoute.name.startsWith('error');
+    }
+  },
+}
 </script>
 
 <template>
-  <header>
+  <header v-if="!this.isNotFoundPage">
     <h1>Welcome To Todo Proj !</h1>
   
   </header>
-  <nav>
+  <nav v-if="!this.isNotFoundPage">
     <ul>
       <li>
         <router-link to="/new">
