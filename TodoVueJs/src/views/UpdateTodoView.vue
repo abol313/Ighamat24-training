b94b05be16212b2d47bd01842038cd26600418c0
abol313 <FazlAbol18@gmail.com>
2022-08-22 16:36:54 +0430

    throw 404 when there is no related todo

--- TodoVueJs/src/views/UpdateTodoView.vue
+++ TodoVueJs/src/views/UpdateTodoView.vue
@@ -5,16 +5,33 @@ import TodoModel from '../scripts/TodoModel.js';
 export default {
     data(){
         return{
-            todo: TodoModel.query().where('id', this.$route.params.id).get()[0]
+            todo: this.getTodo(),
         }
     },
     components:{
         FormUpdateTodo
+    },
+    methods:{
+        throw404Error(){
+            this.$router.push({name:'error-404'});
+        },
+        getTodo(){
+            let todo = TodoModel.query().where('id', this.$route.params.id).get()[0];
+            if(!todo)
+                this.throw404Error();
+
+            return todo;
+        }
+    },
+    beforeCreate(){
+        if(TodoModel.query().where('id', this.$route.params.id).get()[0] === undefined)
+            this.$router.push({name:'error-404'});
+
     }
 }
 
 </script>
 
 <template>
-    <FormUpdateTodo :todo="todo"/>
+    <FormUpdateTodo v-if="todo" :todo="todo"/>
 </template>
