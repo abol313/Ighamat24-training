6ce6c98dd35473ec1f2f51c52c910d46c030b646
abol313 <FazlAbol18@gmail.com>
2022-08-23 19:59:00 +0430

    make TodoFilter component

---
+++ todo_react_js/src/components/TodoFilter.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+export default class TodoFilter extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            filters:{
+                /**
+                 * filter :
+                 * {
+                 *     ! The order of filters in filters object is important
+                 *     ! You should have either pipeItem or pipeAll of every filter
+                 *     pipeEach: item=>item //gets every item and will be passed on every item and return piped item
+                 *     pipeAll: items=>items //gets all items and returns piped items
+                 *     enabled: true/false //this filter pass or not on filtering
+                 *     isIncluded: true/false //use in pipeEach mode and include means in addition or plus others while exclude means just others
+                 * }
+                 */
+                all:{
+                    pipeEach: todo => true,
+                    isIncluded: true,
+                    enabled:false,
+                },
+
+                done:{
+                    pipeEach: todo => todo.done_at!==null,
+                    isIncluded: true,
+                    enabled:false,
+                },
+  
+
+                
+            },
+
+
+
+        };
+    }
+
+    filter(){
+        this.props.filter(this.filterCallback);
+    }
+
+    toggleFilterDone(){
+        this.filters.done.enabled = !this.filters.done.enabled;
+        this.filter();
+    }
+
+
+    filterCallback(orgTodos){
+        let filteredTodos = [];
+        let unfilteredTodos = [...orgTodos];
+
+        // debugger;
+        for(const filter of Object.values(this.state.filters)){
+            if(!filter.enabled)continue;
+
+            if(filter.pipeEach){
+
+                for(let i=0; i<unfilteredTodos.length ; i++){
+
+                    let unfilteredTodo = unfilteredTodos[i];
+
+                    let piped = filter.pipeEach(unfilteredTodo);
+                    //if it is boolean handled existance of item else the returned value from piping will be passed
+                    if(piped===true || piped===false){
+                        if(piped===true){
+                            if(filter.isIncluded)
+                                filteredTodos.push(unfilteredTodo);
+                            unfilteredTodos.splice(i--,1);
+                        }
+                    }else{
+                        filteredTodos.push(piped);
+                        unfilteredTodos.splice(i--,1);
+                    }
+
+                }
+
+                
+            }else if(filter.pipeAll){
+                filteredTodos = filter.pipeAll(filteredTodos);
+            }
+        }
+
+
+        return filteredTodos;
+
+    }
+
+    render(){
+        return (
+            <div class="filter-box">
+                <h2>Filter</h2>
+
+                <label for="filter-dones">Dones</label>
+                <input id="filter-dones" type="checkbox" />
+
+                <label>Date</label>
+                <input type="date" value={new Date()}/>
+            </div>
+        );
+    }
+}
