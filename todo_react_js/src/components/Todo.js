0ccbd150a967e53d20e8cb56019ffe1bc85ad674
abol313 <FazlAbol18@gmail.com>
2022-08-24 01:18:04 +0430

    fix doneAt instiating and handle toggling done of todo

--- todo_react_js/src/components/Todo.js
+++ todo_react_js/src/components/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoModel from '../scripts/TodoModel';
 
 export default class Todo extends React.Component {
     constructor(props){
@@ -8,6 +9,7 @@ export default class Todo extends React.Component {
             id: this.props.todo.id,
             title: this.props.todo.title,
             description: this.props.todo.description,
+            doneAt: this.props.todo.done_at,
 
             dueAt: this.props.todo.due_at,
             createdAt: this.props.created_at,
@@ -16,8 +18,20 @@ export default class Todo extends React.Component {
     }
 
 
-    toggleStatus(){
+    toggleStatus(event){
+        let newDoneAt = !event.target.checked || this.state.doneAt ? null : new Date().toISOString();
+        this.setState({
+            doneAt: newDoneAt
+        });
 
+        console.log('todomodel@update',this.state.id, newDoneAt);
+        TodoModel.update(this.state.id,{
+            done_at: newDoneAt
+        });
+
+        this.props.todo.done_at = newDoneAt;
+        
+        this.props.forceUpdate();
     }
 
     render(){
@@ -27,7 +41,7 @@ export default class Todo extends React.Component {
                 <p className="description">{this.state.description}</p>
                 <p className="due-at">Due at: {this.state.dueAt}</p>
                 <p className="done-at">Done at: {this.state.doneAt}</p>
-                <input type="checkbox" onClick={this.toggleStatus.bind(this)}/>
+                <input type="checkbox" onChange={this.toggleStatus.bind(this)} checked={!!this.state.doneAt}/>
             </div>
         );
     }
