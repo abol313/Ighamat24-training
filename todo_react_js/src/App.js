56b6a110a04852b4833f3454a90f5820d5acc203
abol313 <FazlAbol18@gmail.com>
2022-08-23 12:23:40 +0430

    clean

--- todo_react_js/src/App.js
+++ todo_react_js/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome To Todo Proj !</h1>
       </header>
+
+      <nav v-if="!this.isNotFoundPage">
+        <ul>
+          <li>
+            <router-link to="/new">
+              <h2>Create A Todo</h2>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/">
+              <h2>List Todos</h2>
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+
+
     </div>
   );
 }
--- todo_react_js/src/components/react-c.js
+++
@@ -1,5 +0,0 @@
-function component(props){
-    return <h1>New Component</h1>;
-}
-
-export default component;
