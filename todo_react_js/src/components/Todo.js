192ba351634b6390850a0cd33a64c4a3725db847
abol313 <FazlAbol18@gmail.com>
2022-08-24 17:32:55 +0430

    handle date formatting to output

--- todo_react_js/src/components/Todo.js
+++ todo_react_js/src/components/Todo.js
@@ -12,8 +12,10 @@ export default class Todo extends React.Component {
             doneAt: this.props.todo.done_at,
 
             dueAt: this.props.todo.due_at,
-            createdAt: this.props.created_at,
-            updatedAt: this.props.updated_at,
+            createdAt: this.props.todo.created_at,
+            updatedAt: this.props.todo.updated_at,
+
+            shownCloserTime: false,
         };
     }
 
@@ -34,9 +36,72 @@ export default class Todo extends React.Component {
         this.props.forceUpdate();
     }
 
+    get dueAt(){
+        return this.dateToString(new Date(this.state.dueAt));
+    }
+    get doneAt(){
+        return this.dateToString(new Date(this.state.doneAt));
+    }
+    get lastEditAt(){
+        return this.dateToString(new Date(this.state.updatedAt || this.state.createdAt));
+    }
+    
+    showCloserTime(){
+        this.setState({
+            shownCloserTime: true
+        });
+    }
+
+    showNearlyTime(){
+        this.setState({
+            shownCloserTime: false
+        });
+    }
+
+    dateToString(date, exact = false) {
+        if (!date)
+            return date;
+        exact = this.state.shownCloserTime;
+        // console.log(date);
+        if (exact)
+            return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        let nowDate = new Date();
+        let last_edit_out = "";
+        switch (true) {
+            case nowDate.getFullYear() != date.getFullYear():
+                last_edit_out += `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+                break;
+            case nowDate.getMonth() != date.getMonth():
+                let diffMonth = nowDate.getMonth() - date.getMonth();
+                last_edit_out += `${Math.abs(diffMonth)} month${Math.abs(diffMonth) > 1 ? "s" : ""} ${diffMonth < 0 ? "next" : "ago"}`;
+                break;
+            case nowDate.getDate() != date.getDate():
+                let diffDate = nowDate.getDate() - date.getDate();
+                last_edit_out += `${Math.abs(diffDate)} day${Math.abs(diffDate) > 1 ? "s" : ""} ${diffDate < 0 ? "next" : "ago"}`;
+                break;
+            case nowDate.getHours() != date.getHours():
+                let diffHour = nowDate.getHours() - date.getHours();
+                last_edit_out += `${Math.abs(diffHour)} hour${Math.abs(diffHour) > 1 ? "s" : ""} ${diffHour < 0 ? "next" : "ago"}`;
+                break;
+            case nowDate.getMinutes() != date.getMinutes():
+                let diffMinute = nowDate.getMinutes() - date.getMinutes();
+                last_edit_out += `${Math.abs(diffMinute)} minute${Math.abs(diffMinute) > 1 ? "s" : ""} ${diffMinute < 0 ? "next" : "ago"}`;
+                break;
+            case nowDate.getSeconds() != date.getSeconds():
+                let diffSecond = nowDate.getSeconds() - date.getSeconds();
+                last_edit_out += `${Math.abs(diffSecond)} second${Math.abs(diffSecond) > 1 ? "s" : ""} ${diffSecond < 0 ? "next" : "ago"}`;
+                break;
+            default:
+                last_edit_out += "now";
+        }
+        return last_edit_out;
+    }
+
+    showMore
+
     render(){
         return (
-            <div className="todo-item">
+            <div className="todo-item" onMouseOver={this.showCloserTime.bind(this)} onMouseOut={this.showNearlyTime.bind(this)}>
                 <div className="nav">
                     <p className="title">{this.state.title}</p>
                     <div className="settings">
@@ -46,8 +111,9 @@ export default class Todo extends React.Component {
                     </div>
                 </div>
                 <p className="description">{this.state.description}</p>
-                <p className="due-at">Due at: {this.state.dueAt}</p>
-                <p className="done-at">Done at: {this.state.doneAt}</p>
+                <p className="due-at">Due at: {this.dueAt}</p>
+                <p className="last-edit">Last edit at: {this.lastEditAt}</p>
+                <p className="done-at">{this.state.doneAt? `Done at: ${this.doneAt}`: 'Todo is not done yet!'}</p>
             </div>
         );
     }
