8e0c18f21a7f0c103e36fdfbfad9b24dcfec91bf
abol313 <FazlAbol18@gmail.com>
2022-08-27 08:23:26 +0430

    create App component

---
+++ todo_lit/App.js
@@ -0,0 +1,15 @@
+import { LitElement } from "lit";
+import  ListTodos  from "./src/views/ListTodos";
+export default class App extends LitElement {
+    constructor(){
+
+    }
+
+    render(){
+        return html`
+        
+        `;
+    }
+}
+
+customElements.define('app', App);
