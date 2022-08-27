3991f5d5a6c7f9687bf18a9e47fda9a31c8e791e
abol313 <FazlAbol18@gmail.com>
2022-08-27 13:30:01 +0430

    add event functionalities

--- todo_lit/src/views/ListTodosView.js
+++ todo_lit/src/views/ListTodosView.js
@@ -1,24 +1,35 @@
-import { LitElement } from "lit";
+import { LitElement, html, css, unsafeCSS } from "lit";
+import {repeat} from 'lit/directives/repeat.js';
 import TodoModel from "../scripts/TodoModel";
 import "../components/TodoFilter";
 import "../components/Todo";
+import mainStyle from "../assets/styles/main.scss";
 
 window.TodoModel = TodoModel;
 
-for(let i=0; i<1000; i++){
-    let dueDate = makeRandomlyDate(2022,7);
-    TodoModel.create({
-        title: 'title '+i,
-        description: 'description '+i,
+// for(let i=0; i<100; i++){
+//     let dueDate = makeRandomlyDate(2022,7);
+//     TodoModel.create({
+//         title: 'title '+i,
+//         description: 'description '+i,
+//         due_at: dueDate,
+//         done_at: null,
 
-        due_at: dueDate,
-        done_at: null,
+//         created_at:dueDate - 1e3 * 60 * 60 * 24 * 3,
+//         updated_at:dueDate - 1e3 * 60 * 60 * 24 * 2,
+//     });
+// }
 
-        created_at:dueDate - 1e3 * 60 * 60 * 24 * 3,
-        updated_at:dueDate - 1e3 * 60 * 60 * 24 * 2,
-    });
+function makeRandomlyDate(year=null , month=null, day=null){
+    let date = new Date();
+    date.setFullYear(year || Math.random()*1000+1971);
+    date.setMonth(month || Math.random()*12);
+    date.setDate(day || Math.random()*29);
+    return date;
 }
 export default class ListTodosView extends LitElement {
+    static styles = css`${unsafeCSS(mainStyle)}`;
+
     static properties = {
         orgTodos: {state:true},
         todos: {state:true},
@@ -35,16 +46,21 @@ export default class ListTodosView extends LitElement {
         
     }
 
+    onTodoStatusChanged(){
+        this.requery();
+        this.setFilterCallback(this.filterCallback);
+    }
 
     setFilterCallback(filterCallback){
 
         // debugger;
 
-        console.log(filterCallback(this.todos));
+        console.log('setFilterCallback',filterCallback(this.todos));
         
         this.filterCallback = filterCallback;
 
         // this.forceUpdate();
+        // this.requestUpdate();
 
         return this;
     }
@@ -66,21 +82,27 @@ export default class ListTodosView extends LitElement {
         return this.filterCallback(this.todos);
     }
 
-    getTodosElements(){
-        return this.getTodos().map( todo => 
-            (<Todo key={todo.id} todo={todo} searchText={this.searchText} forceUpdate={this.forceUpdate.bind(this)} onRequery={this.requery.bind(this)}/>)
-        );
-    }
+    // getTodosElements(){
+    //     return this.getTodos().map( todo => 
+    //         (<Todo key={todo.id} todo={todo} searchText={this.searchText} forceUpdate={this.forceUpdate.bind(this)} onRequery={this.requery.bind(this)}/>)
+    //     );
+    // }
 
 
     getTodosElements(){
+        repeat
         return html`
-            ${ this.getTodos().map( todo => html`<todo-item todo="${JSON.stringify(todo)}" @forceUpdate="${()=>this.requestUpdate()}"></todo-item>` ) }
+            ${ repeat(
+                this.getTodos(), 
+                todo=>todo.id,
+                todo => html`<todo-item class="todo-item" todo="${JSON.stringify(todo)}" @force-update=${this.onTodoStatusChanged}></todo-item>`
+            ) }
         `;
     }
 
     onFilter(event){
-        this.setFilterCallback(event.filterCallback);
+        console.log('list-todos-view@onFilter',event);
+        this.setFilterCallback(event.detail.filterCallback);
     }
 
     onSearch(event){
@@ -89,7 +111,7 @@ export default class ListTodosView extends LitElement {
 
     render(){
         return html`
-            <todo-filter onFilter="${this.onFilter}"></todo-filter>
+            <todo-filter @filter=${this.onFilter} ></todo-filter>
 
             <div class="todos">
                 ${this.getTodosElements()}
