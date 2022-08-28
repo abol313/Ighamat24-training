dd804e0e030e14653437dd46c1c897a91a92f7f7
abol313 <FazlAbol18@gmail.com>
2022-08-28 13:31:12 +0430

    reload libraries and routes

--- blog_sqlite3/public/auto_reload.php
+++ blog_sqlite3/public/auto_reload.php
@@ -1,4 +1,5 @@
 <?php
-require_once(__DIR__."/../app/models/Model.php");
+foreach(glob(__DIR__."/../libraries/*.php") as $path) require_once($path);
 foreach(glob(__DIR__."/../app/models/*.php") as $path) require_once($path);
 foreach(glob(__DIR__."/../app/database/*.php") as $path) require_once($path);
+foreach(glob(__DIR__."/../routes/*.php") as $path) require_once($path);
