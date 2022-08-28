1114e012c9723a4280d91e5173ee17e9bd0880e6
abol313 <FazlAbol18@gmail.com>
2022-08-28 15:03:08 +0430

    fix $viewsDir and add strLimitLength helper

--- blog_sqlite3/libraries/helpers.php
+++ blog_sqlite3/libraries/helpers.php
@@ -1,6 +1,14 @@
 <?php
-$viewsDir = __DIR__."../views";
+$viewsDir = __DIR__."/../views";
 function view($viewName){
     global $viewsDir;
     return require_once($viewsDir."/".str_replace('.', '/', $viewName).".abol.php");
+}
+
+
+function strLimitLength(string $str,int $max,string $exceed="..."){
+    $out = mb_substr($str, 0, $max);
+    if(mb_strlen($str)>$max)
+        $out = mb_substr($out, 0, -mb_strlen($exceed)) . $exceed;
+    return $out;
 }
