f214f04ef6bf5c6e4b07b8813beecfae21dacdfe
abol313 <FazlAbol18@gmail.com>
2022-08-28 17:45:03 +0430

    handle http Requests

---
+++ blog_sqlite3/libraries/Request.php
@@ -0,0 +1,23 @@
+<?php
+namespace Abol\Http\Request;
+
+class Request {
+    protected string $uri;
+    protected array $inputs;
+    protected array $params;
+
+    public function __construct($uri = null, $inputs = null, $params = null){
+        $this->uri = $uri ?? $_SERVER['REQUEST_URI'];
+        $this->inputs = $inputs ?? $_REQUEST;
+        $this->params = $params;
+    }
+
+
+    public function input($name, $default=null){
+        return $this->inputs[$name] ?? $default;
+    }
+
+    public function param($name, $default=null){
+        return $this->params[$name] ?? $default;
+    }
+}
