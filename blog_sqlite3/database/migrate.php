00e06a7c3c4055a8c796210fac2286a3379e0df2
abol313 <FazlAbol18@gmail.com>
2022-08-28 09:43:04 +0430

    comment info

--- blog_sqlite3/database/migrate.php
+++ blog_sqlite3/database/migrate.php
@@ -1,4 +1,7 @@
 <?php
+//! If there is no .db (database) file you can run it file as php,
+// then the database file will automatically create
+
 echo "running migration...\n";
 foreach(glob(__DIR__."/migrations/*.php") as $migration){
     // echo $migration;
