bc6048b786212d0c506345297dd6091f3dff701b
abol313 <FazlAbol18@gmail.com>
2022-08-28 12:58:12 +0430

    require first the model

--- blog_sqlite3/public/auto_reload.php
+++ blog_sqlite3/public/auto_reload.php
@@ -1,4 +1,4 @@
 <?php
-use \SQLite3;
-foreach(glob("../app/models/*.php") as $path) require_once($path);
-foreach(glob("../app/database/*.php") as $path) require_once($path);
+require_once(__DIR__."/../app/models/Model.php");
+foreach(glob(__DIR__."/../app/models/*.php") as $path) require_once($path);
+foreach(glob(__DIR__."/../app/database/*.php") as $path) require_once($path);
