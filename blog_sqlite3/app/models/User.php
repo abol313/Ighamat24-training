81c18775658ca9ae8db3906d213401fc13f665f7
abol313 <FazlAbol18@gmail.com>
2022-08-28 08:57:02 +0430

    handle connect to db and all method

--- blog_sqlite3/app/models/User.php
+++ blog_sqlite3/app/models/User.php
@@ -1,3 +1,25 @@
 <?php
 namespace App\Models;
 
+use SQLite3;
+
+class User extends SQLite3 {
+    public $keyName = 'id';
+    protected $table = 'users';
+
+    function __construct(){
+        $this->open("../database/blog.db");
+    }
+
+    function all($columns = ['*']){
+        $wildcards = implode(' ',array_map(fn($column)=>'?', $columns));
+        $statement = $this->prepare("SELECT $wildcards FROM $this->table");
+        for($i = 0; $i < count($columns); $i++)
+            $statement->bindValue($i, $columns[$i]);
+        return $statement->execute();
+    }
+
+    function create($record){
+        
+    }
+}
