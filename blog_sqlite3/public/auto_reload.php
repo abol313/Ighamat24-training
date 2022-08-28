d39368f3d9b3df2759abb48c46d3cc62aa38208b
abol313 <FazlAbol18@gmail.com>
2022-08-28 15:21:23 +0430

    reload auto controllers too

--- blog_sqlite3/public/auto_reload.php
+++ blog_sqlite3/public/auto_reload.php
@@ -2,4 +2,5 @@
 foreach(glob(__DIR__."/../libraries/*.php") as $path) require_once($path);
 foreach(glob(__DIR__."/../app/models/*.php") as $path) require_once($path);
 foreach(glob(__DIR__."/../app/database/*.php") as $path) require_once($path);
+foreach(glob(__DIR__."/../app/http/controllers/*.php") as $path) require_once($path);
 foreach(glob(__DIR__."/../routes/*.php") as $path) require_once($path);
