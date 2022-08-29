a15e5f9e9c0faad794361442d5166479aaeac417
abol313 <FazlAbol18@gmail.com>
2022-08-29 10:50:22 +0430

    handle the posts.show when there is not related post

--- blog_sqlite3/views/posts/show.abol.php
+++ blog_sqlite3/views/posts/show.abol.php
@@ -2,10 +2,14 @@
 
 
 <ul>
+    <?php if($post){?>
     <li>
         <h2><?= $post['title']?></h2>
         <p><?= $post['description'] ?></p>
     </li>
+    <?php }else{ ?>
+        <h2>There is no same post !</h2>
+    <?php } ?>
 </ul>
 
 <?php view("footer");?>
