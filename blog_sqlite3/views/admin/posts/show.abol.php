e91f9d64aa0051ec36fc70de889cfacbd917b8f0
abol313 <FazlAbol18@gmail.com>
2022-08-29 10:49:24 +0430

    handle when there is not the post in admin.posts.show

--- blog_sqlite3/views/admin/posts/show.abol.php
+++ blog_sqlite3/views/admin/posts/show.abol.php
@@ -2,12 +2,16 @@
 
 
 <ul>
+    <?php if($post){?>
     <li>
         <h2><?= $post['title']?></h2>
         <p><?= $post['description'] ?></p>
         <a href="<?=route('admin.posts.edit', ['id'=>$post['id']])?>">Edit</a>
-        <a href="<?=route('admin.posts.delete', ['id'=>$post['id']])?>">Delete</a>
+        <a href="<?=route('admin.posts.destroy', ['id'=>$post['id']])?>">Delete</a>
     </li>
+    <?php }else{ ?>
+        <h2>There is no same post !</h2>
+    <?php } ?>
 </ul>
 
 <?php view("footer");?>
