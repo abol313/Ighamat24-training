4f3d76aed69e9cd3197983fb9c2449552496c27e
abol313 <FazlAbol18@gmail.com>
2022-08-29 10:07:46 +0430

    add related routes of blog

--- blog_sqlite3/routes/web.php
+++ blog_sqlite3/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Abol\Router\Router;
+use App\Http\Controllers\PostController;
 
-Router::addRoute('/home',function(){
-    view('home');
-});
+
+Router::addRoute('/posts',[PostController::class,'index'])->name('posts.index');
+Router::addRoute('/owner/posts',[PostController::class,'adminIndex'])->name('admin.posts.index');
+
+Router::addRoute('/owner/posts/create',[PostController::class,'create'])->name('admin.posts.create');
+Router::addRoute('/owner/posts/store',[PostController::class,'store'])->name('admin.posts.store');
+
+Router::addRoute('/owner/posts/edit',[PostController::class,'edit'])->name('admin.posts.edit');
+Router::addRoute('/owner/posts/update',[PostController::class,'update'])->name('admin.posts.update');
+
+Router::addRoute('/posts/{id}',[PostController::class,'show'])->name('posts.show');
+Router::addRoute('/owner/posts/{id}',[PostController::class,'adminShow'])->name('admin.posts.show');
+
