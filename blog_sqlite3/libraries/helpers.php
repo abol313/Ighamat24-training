004f8e143a596c8ece1b3da0ff5b5ca796bd221b
abol313 <FazlAbol18@gmail.com>
2022-08-29 09:24:45 +0430

    pass data on view

--- blog_sqlite3/libraries/helpers.php
+++ blog_sqlite3/libraries/helpers.php
@@ -1,7 +1,14 @@
 <?php
+
+use Abol\Router\Router;
+
 $viewsDir = __DIR__."/../views";
-function view($viewName){
+function view($viewName, $data=[]){
     global $viewsDir;
+
+    foreach($data as $key => $value)
+        $$key = $value;
+
     return require_once($viewsDir."/".str_replace('.', '/', $viewName).".abol.php");
 }
 
@@ -11,4 +18,21 @@ function strLimitLength(string $str,int $max,string $exceed="..."){
     if(mb_strlen($str)>$max)
         $out = mb_substr($out, 0, -mb_strlen($exceed)) . $exceed;
     return $out;
+}
+
+function redirectTo($uri, $query=null){
+    $uri = trim($uri, '/');
+    $queryRaw = $query ? "?".http_build_query($query) : null;
+    $host = $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'];
+    $location = $uri.$queryRaw;
+    header("Location: $location");
+    die();
+}
+
+function route($name, $params=[]){
+    $closure = Router::getRouteOfName($name);
+    
+    $uri = $closure($params);
+
+    redirectTo($uri);
 }
