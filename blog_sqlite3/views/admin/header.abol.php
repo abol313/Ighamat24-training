944772043fcd98a783f3dbef984637b30c604c79
abol313 <FazlAbol18@gmail.com>
2022-08-29 10:44:47 +0430

    make navigations to admin home and create post routes

--- blog_sqlite3/views/admin/header.abol.php
+++ blog_sqlite3/views/admin/header.abol.php
@@ -1,6 +1,11 @@
 <header>
     <h1>Blog [Admin]</h1>
     <ul>
-        <li>Home</li>
+        <li>
+            <a href="<?=route('admin.posts.index')?>">Admin Home</a>
+        </li>
+        <li>
+            <a href="<?=route('admin.posts.create')?>">Create Post</a>
+        </li>
     </ul>
 </header>
