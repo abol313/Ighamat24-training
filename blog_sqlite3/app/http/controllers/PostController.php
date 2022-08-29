e5bae3bcc83ece5bfc5d37421e66958c2abb8ce9
abol313 <FazlAbol18@gmail.com>
2022-08-29 10:08:01 +0430

    handle related actions

--- blog_sqlite3/app/http/controllers/PostController.php
+++ blog_sqlite3/app/http/controllers/PostController.php
@@ -1,8 +1,54 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostController {
-    public function show(){
-        view('header');
+    public function index(){
+        view('posts.index');
     }
+
+    public function adminIndex(){
+        view('admin.posts.index');
+    }
+
+    public function show($request, $id){
+        $post = (new Post())->find($id);
+        view('posts.show', ['post'=>$post]);
+    }
+
+    public function adminShow($request, $id){
+        $post = (new Post())->find($id);
+        view('admin.posts.show', ['post'=>$post]);
+    }
+
+    public function create(){
+        view('admin.posts.create');
+    }
+
+    public function store($request){
+        $post = new Post();
+        $post->create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+        ]);
+
+        var_dump($_SERVER);
+    }
+
+    public function edit($request, $id){
+        $post = (new Post())->find($id);
+        view('admin.posts.edit', ['post'=>$post]);
+    }
+
+    public function update($request){
+        $post = new Post();
+        $post->update($request->input('id'),[
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+        ]);
+
+        var_dump($_SERVER);
+    }
+
 }
