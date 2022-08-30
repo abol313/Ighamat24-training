d20ff4c45f9384f6abb5fd7724141d8f5b751d20
abol313 <FazlAbol18@gmail.com>
2022-08-30 11:05:12 +0430

    handle the infos view

--- seo_automation/resources/views/seo.blade.php
+++ seo_automation/resources/views/seo.blade.php
@@ -7,9 +7,72 @@
     <title>Seo Check</title>
 </head>
 <body>
-    @foreach($info as $title=>$description)
-        <p>{{$title}}</p>
-        <pre><?php print_r($description)?></pre>
+
+    @foreach($infos as $info)
+        <div class="info-item" onclick="this.classList.toggle('info-item-clicked')">
+            <h2 class="url">{{$info['url']}}</h2>
+            <div class="desc">
+                @if($info['has-importants'])
+                    <h2 class="ok">It has title, meta description, and canonical</h2>
+                @else
+                    <h2 class="warning">This link has seo problems!</h2>
+                @endif
+
+                @if($info['title'])
+                    <h3 class="title ok">Title : {{$info['title']}}</h3>
+                @else
+                    <h3 class="title warning">No Title !</h3>
+                @endif
+
+                
+                @if($info['meta-description'])
+                    <h3 class="meta-description ok">Meta Description : {{$info['meta-description']}}</h3>
+                @else
+                    <h3 class="meta-description warning">No Meta Description !</h3>
+                @endif
+
+                
+                @if($info['canonical'])
+                    <h3 class="canonical ok">Canonical : {{$info['canonical']}}</h3>
+                @else
+                    <h3 class="canonical warning">No Canonical !</h3>
+                @endif
+
+                
+
+                <div class="links">
+                    <h3>Links : </h3>
+                    @foreach($info['links'] as $link)
+                        <a href="{{$link}}">
+                            <p class="link">{{$link}}</p>
+                        </a>
+                    @endforeach
+                </div>
+                <hr/>
+
+                <div class="images">
+                    <h3>Images : </h3>
+                    @foreach($info['images'] as $image)
+                        <a href="{{$image}}">
+                            <p class="image">{{$image}}</p>
+                        </a>
+                    @endforeach
+                </div>
+                <hr/>
+
+                <div class="videos">
+                    <h3>Videos : </h3>
+                    @foreach($info['videos'] as $video)
+                        <a href="{{$video}}">
+                            <p class="video">{{$video}}</p>
+                        </a>
+                    @endforeach
+                </div>
+                <hr/>
+
+                
+            </pre>
+        </div>
     @endforeach
 
 </body>
