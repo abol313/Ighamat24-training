b6b10f718b1be172837f7ea3f86b91adb1c8de70
abol313 <FazlAbol18@gmail.com>
2022-09-07 21:54:34 +0430

    add and handle customer controller

---
+++ installment/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \App\Models\Customer;
+use \App\Models\Server;
+use \App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerController extends Controller
+{
+    //
+    public function register(){
+        return view('customers.register');
+    }
+    public function signUp(Request $request){
+
+        $customer = Customer::create([
+            'username' => $request->input('username'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        Auth::guard('customers')->login($customer);
+        return back();
+    }
+
+    
+    public function login(){
+        return view('customers.login');
+    }
+    public function signIn(Request $request){
+        Auth::guard('customers')->attempt(
+            $request->only([
+                'username',
+                'password',
+            ])
+        );
+
+        return back();
+    }
+
+    
+}
