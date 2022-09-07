3ef1398fc7145f2f62f34ea2f0e453760fa653f9
abol313 <FazlAbol18@gmail.com>
2022-09-07 22:41:07 +0430

    add server auth routes

--- installment/routes/web.php
+++ installment/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ServerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +25,9 @@ Route::get('/customers/register', [CustomerController::class, 'register']);
 Route::post('/customers/register', [CustomerController::class, 'signUp'])->name('customers.sign_up');
 Route::get('/customers/login', [CustomerController::class, 'login']);
 Route::post('/customers/login', [CustomerController::class, 'signIn'])->name('customers.sign_in');
+
+// server auth
+Route::get('/servers/register', [ServerController::class, 'register']);
+Route::post('/servers/register', [ServerController::class, 'signUp'])->name('servers.sign_up');
+Route::get('/servers/login', [ServerController::class, 'login']);
+Route::post('/servers/login', [ServerController::class, 'signIn'])->name('servers.sign_in');
