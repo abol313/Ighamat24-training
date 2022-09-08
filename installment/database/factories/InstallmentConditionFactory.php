1c6100f3acc1fa2ba2f99331d7424b535bacd58f
abol313 <FazlAbol18@gmail.com>
2022-09-08 15:43:08 +0430

    make installment condition factory

---
+++ installment/database/factories/InstallmentConditionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Service;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InstallmentCondition>
+ */
+class InstallmentConditionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'maximum' => $this->faker->numberBetween(1e6, 1e9),
+            'installments_count' => $this->faker->numberBetween(2, 20),
+            'service_id' => Service::all()->random()['id'],
+        ];
+    }
+}
