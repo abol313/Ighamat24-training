6b5ad3cc6b624db9451dfdb2f427858f0d5fac9e
abol313 <FazlAbol18@gmail.com>
2022-09-08 11:01:36 +0430

    register policies

--- installment/app/Providers/AuthServiceProvider.php
+++ installment/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Service;
+use App\Policies\ServicePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Service::class => ServicePolicy::class
     ];
 
     /**
