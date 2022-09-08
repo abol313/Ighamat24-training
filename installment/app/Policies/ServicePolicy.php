8145dab24078b29894e122be0926549b63805d01
abol313 <FazlAbol18@gmail.com>
2022-09-08 11:01:08 +0430

    handle the ServicePolicy

---
+++ installment/app/Policies/ServicePolicy.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Server;
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Auth;
+
+class ServicePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(Server $server)
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(?Server $server, Service $service)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(?Server $server)
+    {
+        //
+        
+        return Auth::guard('servers')->check();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(Server $server, Service $service)
+    {
+        //
+        
+        return $service->server_id === $server->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(Server $server, Service $service)
+    {
+        //
+
+        return $service->server_id === $server->id;
+
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(?Server $server, Service $service)
+    {
+        //
+
+        return $service->server_id === $server->id;
+
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(?Server $server, Service $service)
+    {
+        //
+ 
+        return $service->server_id === $server->id;
+
+    }
+}
