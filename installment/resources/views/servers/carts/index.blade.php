ba7d3cae5cf230587887d4dc6bf961f99b1f220e
abol313 <FazlAbol18@gmail.com>
2022-09-08 23:39:06 +0430

    handle server pending carts views

---
+++ installment/resources/views/servers/carts/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Server Carts [Pending to Accept/Reject]</h2>
+
+    @foreach($carts as $cart)
+        <div class="box">
+            <p> installments count : {{$cart->installments_no}}, <a href="{{route('services.installment_conditions.show', [$cart->service, $cart->installmentCondition])}}">Installment condition</a></p>
+            <p><a href="{{route('services.show', [$cart->service])}}"> service : {{$cart->service->title}}</a></p>
+            <p> amount : {{$cart->amount.$cart->service->unit}}</p>
+            <p> price : {{$cart->price}}</p>
+            <p> status : {{$cart->status}}</p>
+            <p> customer : {{$cart->customer->username}}</p>
+
+            <form action="{{route('servers.carts.accept', [$server, $cart])}}" method="POST">
+                @csrf
+                <input type="submit" value="accept"/>
+            </form>
+
+            <form action="{{route('servers.carts.reject', [$server, $cart])}}" method="POST">
+                @csrf
+                <input type="submit" value="reject"/>
+            </form>
+        </div>
+
+    @endforeach
+@endsection
