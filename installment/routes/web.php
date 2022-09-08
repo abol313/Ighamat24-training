3d4ecc6b01974c1d0df059be935df8750714b1df
abol313 <FazlAbol18@gmail.com>
2022-09-08 11:14:09 +0430

    name some of routes to refer to header of app

--- installment/routes/web.php
+++ installment/routes/web.php
@@ -22,19 +22,19 @@ Route::get('/', function () {
 
 
 // customer auth
-Route::get('/customers/register', [CustomerController::class, 'register']);
+Route::get('/customers/register', [CustomerController::class, 'register'])->name('customers.register');
 Route::post('/customers/register', [CustomerController::class, 'signUp'])->name('customers.sign_up');
-Route::get('/customers/login', [CustomerController::class, 'login']);
+Route::get('/customers/login', [CustomerController::class, 'login'])->name('customers.login');
 Route::post('/customers/login', [CustomerController::class, 'signIn'])->name('customers.sign_in');
-Route::get('/customers/logout', [CustomerController::class, 'logout']);
+Route::get('/customers/logout', [CustomerController::class, 'logout'])->name('customers.logout');
 
 
 // server auth
-Route::get('/servers/register', [ServerController::class, 'register']);
+Route::get('/servers/register', [ServerController::class, 'register'])->name('servers.register');
 Route::post('/servers/register', [ServerController::class, 'signUp'])->name('servers.sign_up');
-Route::get('/servers/login', [ServerController::class, 'login']);
+Route::get('/servers/login', [ServerController::class, 'login'])->name('servers.login');
 Route::post('/servers/login', [ServerController::class, 'signIn'])->name('servers.sign_in');
-Route::get('/servers/logout', [ServerController::class, 'logout']);
+Route::get('/servers/logout', [ServerController::class, 'logout'])->name('servers.logout');
 
 
 // service resoure routes
