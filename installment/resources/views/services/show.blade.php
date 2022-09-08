627380861afca7894b00c996442a985ea0c704fa
abol313 <FazlAbol18@gmail.com>
2022-09-08 15:47:32 +0430

    handle auth and related installment conditions

--- installment/resources/views/services/show.blade.php
+++ installment/resources/views/services/show.blade.php
@@ -10,6 +10,23 @@
             <p>server : {{$service->server->username}}</p>
             <p>unit : {{$service->unit}}</p>
             <p>price_per_unit : {{$service->price_per_unit}}</p>
+
+            <p>
+                <a href="{{route('services.installment_conditions.index', [$service])}}">
+                    Show installment conditions
+                </a>
+            </p>
+
+            @if(auth('servers')->user()?->can('update',$service))
+                <a href="{{route('services.edit', [$service])}}">edit service</a>
+            @endif
+            @if(auth('servers')->user()?->can('delete',$service))
+                <form action="{{route('services.destroy', [$service])}}" method="POST">
+                    @csrf
+                    @method('delete')
+                    <input type="submit" value="delete"/>
+                </form>
+            @endif
     </div>
 
 @endsection
