77a57461e9ced1e3742e935f8b3a1480e6081fe1
abol313 <FazlAbol18@gmail.com>
2022-09-08 11:02:59 +0430

    add logout routes and my services

--- installment/routes/web.php
+++ installment/routes/web.php
@@ -26,12 +26,17 @@ Route::get('/customers/register', [CustomerController::class, 'register']);
 Route::post('/customers/register', [CustomerController::class, 'signUp'])->name('customers.sign_up');
 Route::get('/customers/login', [CustomerController::class, 'login']);
 Route::post('/customers/login', [CustomerController::class, 'signIn'])->name('customers.sign_in');
+Route::get('/customers/logout', [CustomerController::class, 'logout']);
+
 
 // server auth
 Route::get('/servers/register', [ServerController::class, 'register']);
 Route::post('/servers/register', [ServerController::class, 'signUp'])->name('servers.sign_up');
 Route::get('/servers/login', [ServerController::class, 'login']);
 Route::post('/servers/login', [ServerController::class, 'signIn'])->name('servers.sign_in');
+Route::get('/servers/logout', [ServerController::class, 'logout']);
+
 
 // service resoure routes
+Route::get('/services/me', [ServiceController::class, 'indexMyServices']);
 Route::resource('services', ServiceController::class);
