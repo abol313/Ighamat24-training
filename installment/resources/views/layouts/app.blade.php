3f4ddb3698d8d8902d6e55bb3ecbdcd7348afbcb
abol313 <FazlAbol18@gmail.com>
2022-09-09 11:22:42 +0430

    handle header references links

--- installment/resources/views/layouts/app.blade.php
+++ installment/resources/views/layouts/app.blade.php
@@ -15,7 +15,15 @@
     </style>
     <header>
         @auth('customers')
-            <h2>Hey [{{auth('customers')->user()->username}}], you are a Customer, <a href="{{route('customers.logout')}}">logout customer</a></h2>
+            <div class="box">
+                <h2>Hey [{{auth('customers')->user()->username}}], you are a Customer, <a href="{{route('customers.logout')}}">logout customer</a></h2>
+            
+                <p>
+                    <a href="{{route('customers.carts.index', [auth('customers')->user()])}}">List My Carts</a>
+                </p>
+
+
+            </div>
         @else
             <div>
                 <a href="{{route('customers.login')}}">Login Customer</a>
@@ -24,7 +32,24 @@
         @endauth
 
         @auth('servers')
-            <h2>Hey [{{auth('servers')->user()->username}}], you are a Server, <a href="{{route('servers.logout')}}">logout server</a></h2>
+            <div class="box">
+                <h2>Hey [{{auth('servers')->user()->username}}], you are a Server, <a href="{{route('servers.logout')}}">logout server</a></h2>
+                @if(auth('servers')->user()?->can('create', \App\Models\Service::class))
+                    <p>
+                        <a href="{{route('services.create')}}">Create Service</a>
+                    </p>
+                @endif
+                <p>
+                    <a href="{{route('services.index_my_services')}}">List My Services</a>
+                </p>
+                <p>
+                    <a href="{{route('servers.carts.index', [auth('servers')->user()])}}">List Server's Carts</a>
+                </p>
+                <p>
+                    <a href="{{route('servers.installments.index', [auth('servers')->user()])}}">List Server's Installments</a>
+                </p>
+
+            </div>
         @else
             <div>
                 <a href="{{route('servers.login')}}">Login Server</a>
@@ -37,20 +62,7 @@
             <p>
                 <a href="{{route('services.index')}}">List Services</a>
             </p>
-            @auth('servers')
-                <p>
-                    <a href="{{route('services.index_my_services')}}">List My Services</a>
-                </p>
-            @endauth
-            @if(auth('servers')->user()?->can('create', \App\Models\Service::class))
-                <p>
-                    <a href="{{route('services.create')}}">Create Services</a>
-                </p>
-                <p>
-                    <a href="{{route('servers.carts.index', [auth('servers')->user()])}}">List Server's Pending Carts</a>
-                </p>
-            
-            @endif
+
         </div>
     </header>
 
