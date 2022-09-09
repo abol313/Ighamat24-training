2e9474d98fbbd3be5fe72d5bb49a63ea3afce429
abol313 <FazlAbol18@gmail.com>
2022-09-09 11:18:09 +0430

    handle servers requested carts views

--- installment/resources/views/servers/carts/index.blade.php
+++ installment/resources/views/servers/carts/index.blade.php
@@ -6,21 +6,27 @@
     @foreach($carts as $cart)
         <div class="box">
             <p> installments count : {{$cart->installments_no}}, <a href="{{route('services.installment_conditions.show', [$cart->service, $cart->installmentCondition])}}">Installment condition</a></p>
+            <p>
+                <a href="{{route('servers.installments.index', [$server, 'cart_id'=>$cart])}}">Server Installments</a>
+            </p>
             <p><a href="{{route('services.show', [$cart->service])}}"> service : {{$cart->service->title}}</a></p>
             <p> amount : {{$cart->amount.$cart->service->unit}}</p>
             <p> price : {{$cart->price}}</p>
             <p> status : {{$cart->status}}</p>
             <p> customer : {{$cart->customer->username}}</p>
 
-            <form action="{{route('servers.carts.accept', [$server, $cart])}}" method="POST">
-                @csrf
-                <input type="submit" value="accept"/>
-            </form>
+            @if($cart->status==='pending')
+                <form action="{{route('servers.carts.accept', [$server, $cart])}}" method="POST">
+                    @csrf
+                    <input type="submit" value="accept"/>
+                </form>
 
-            <form action="{{route('servers.carts.reject', [$server, $cart])}}" method="POST">
-                @csrf
-                <input type="submit" value="reject"/>
-            </form>
+                <form action="{{route('servers.carts.reject', [$server, $cart])}}" method="POST">
+                    @csrf
+                    <input type="submit" value="reject"/>
+                </form>
+            @endif
+
         </div>
 
     @endforeach
--- installment/resources/views/servers/carts/show.blade.php
+++ installment/resources/views/servers/carts/show.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Customer Carts</h2>
+    <h2>Server Carts [Pending to Accept/Reject]</h2>
 
     <div class="box">
         <p> installments count : {{$cart->installments_no}}, <a href="{{route('services.installment_conditions.show', [$cart->service, $cart->installmentCondition])}}">Installment condition</a></p>
+        <p>
+            <a href="{{route('servers.installments.index', [$server, 'cart_id'=>$cart])}}">Server Installments</a>
+        </p>
         <p><a href="{{route('services.show', [$cart->service])}}"> service : {{$cart->service->title}}</a></p>
         <p> amount : {{$cart->amount.$cart->service->unit}}</p>
         <p> price : {{$cart->price}}</p>
@@ -13,15 +16,17 @@
 
 
         
-        <form action="{{route('servers.carts.accept', [$server, $cart])}}" method="POST">
-            @csrf
-            <input type="submit" value="accept"/>
-        </form>
+        @if($cart->status==='pending')
+            <form action="{{route('servers.carts.accept', [$server, $cart])}}" method="POST">
+                @csrf
+                <input type="submit" value="accept"/>
+            </form>
 
-        <form action="{{route('servers.carts.reject', [$server, $cart])}}" method="POST">
-            @csrf
-            <input type="submit" value="reject"/>
-        </form>
+            <form action="{{route('servers.carts.reject', [$server, $cart])}}" method="POST">
+                @csrf
+                <input type="submit" value="reject"/>
+            </form>
+        @endif
     </div>
 
 @endsection
