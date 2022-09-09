048afa06d683895adf127b25c387a241eca2fd5b
abol313 <FazlAbol18@gmail.com>
2022-09-09 11:18:51 +0430

    handle servers requested carts installments accept or not

---
+++ installment/resources/views/servers/carts/installments/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    @if($cart ?? null)
+        <h2><a href="{{route('servers.carts.show', [$server, $cart])}}">Cart</a> Installments</h2>
+    @else
+        <h2>Installments</h2>
+    @endif
+
+    @foreach($installments as $installment)
+        <div class="box">
+            <p><a href="{{route('servers.carts.show', [$server, $installment->cart])}}">Go To Cart</a></p>
+            <p> price : {{$installment->price}}</p>
+            @if($installment->paid_at)
+                <p>pay status : paid</p>
+            @else
+                <p>pay status : un-paid</p>
+            @endif
+    
+            @if($installment->server_accepted_at)
+                <p>accept status : accepted</p>
+            @else
+                <p>accept status : not-accepted</p>
+            @endif
+
+            @if(!$installment->server_accepted_at)
+                <form action="{{route('servers.installments.accept', [$server, $installment])}}" method="post">
+                    @csrf
+                    <input type="submit" value="Accept Paid Installment"/>
+                </form>
+            @endif
+
+        </div>
+
+    @endforeach
+@endsection
